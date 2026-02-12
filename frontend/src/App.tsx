f8fc3374778c2439d12bd7b71d013206d078ea20
Shamshyrak Zholdasbek <shamshyrak.zholdasbek@gmail.com>
2026-02-12 21:52:26 +0500

    feat: remove new dataset page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CreateDatasetPage from "./pages/CreateDatasetPage";
 import DatasetsPage from "./pages/DatasetsPage";
 import GeneratePage from "./pages/GeneratePage";
 import RunSchedulePage from "./pages/RunSchedulePage";
@@ -19,7 +18,6 @@ export default function App() {
           <Routes>
             <Route path="/" element={<GeneratePage />} />
             <Route path="/datasets" element={<DatasetsPage />} />
-            <Route path="/datasets/new" element={<CreateDatasetPage />} />
             <Route path="/runs" element={<RunsPage />} />
             <Route path="/runs/:runId/schedule" element={<RunSchedulePage />} />
           </Routes>
